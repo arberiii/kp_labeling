1fb8acd384729c8b946b7344e2f84eec3e9c591f
arberiii <arber_919@live.com>
2019-05-13 17:49:24 +0200

    write to file

--- graph.go
+++ graph.go
@@ -1,14 +1,40 @@
 package main
 
-type cycle []int
+import "os"
 
-func createCycle(n int) cycle {
-	c := make([]int, n)
-	return c
+type cycle struct {
+	labels []string
+	length int
 }
 
-func (c cycle) labelFirstTwoEdges() {
-	c[0] = 1
-	c[1] = 2
+func createCycle(n int) cycle {
+	c := make([]string, n)
+	return cycle{labels: c, length: n}
+}
+
+func (c *cycle) labelFirstTwoEdges() {
+	c.labels[0] = ""
+	c.labels[1] = ""
 	return
+}
+
+func (c *cycle) String() string {
+	ret := c.labels[0]
+	for i := 1; i < c.length; i++ {
+		ret = ret + ", " + c.labels[i]
+	}
+	return ret
+}
+
+func (c *cycle) writeToFile() {
+	f, err := os.OpenFile("cycle", os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
+
+	if _, err = f.WriteString(c.String()); err != nil {
+		panic(err)
+	}
 }
