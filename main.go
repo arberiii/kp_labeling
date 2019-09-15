635083f313a696f24c15e2f0e5c51365de4cfa96
arberiii <arber_919@live.com>
2019-09-15 12:57:39 +0200

    main

--- main.go
+++ main.go
@@ -1,13 +1,4 @@
 package main
 
-import "fmt"
 
-func main() {
-	c := newCycle(8)
-	c.labelFirstTwoEdges()
-	c.nextLabel(1)
-	fmt.Println(c)
-	fmt.Println(labelsOrderSet(c.labels))
-	fmt.Println(c)
-	fmt.Println(c.partition(2))
-}
+func main() {}
